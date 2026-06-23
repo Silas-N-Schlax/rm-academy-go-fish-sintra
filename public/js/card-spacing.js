3d29ea67683d80639e735197a707aeda5b1af6bf
SilasNS <silasschlax@gmail.com>
2026-06-23 08:49:15 -0400

    Copy of script for card overlapping

---
+++ public/js/card-spacing.js
@@ -0,0 +1,35 @@
+document.addEventListener('DOMContentLoaded', () => {
+  changeOffset()
+})
+
+window.addEventListener('resize', () => {
+  changeOffset()
+})
+
+
+function changeOffset() {
+  const cardCollections = document.querySelectorAll('.card-collection')
+  console.log(cardCollections)
+  cardCollections.forEach((collection) => {
+    cards = collection.querySelectorAll('.playing-card')
+
+    let minOffset = 15 // desktop min offset
+    if (window.innerWidth < 800) minOffset = 5 // mobile min offset
+
+    const cardWidth = cards[1].width
+    const cardsLength = cards.length
+    let containerWidth = collection.offsetWidth
+
+    let offset = 0
+    while ((cardsLength * cardWidth) - (offset * cardsLength) > containerWidth - cardWidth) {
+      offset += 1
+    }
+
+    if (offset < minOffset) offset = minOffset
+
+    collection.style.marginLeft = `${offset}px`
+    cards.forEach((card) => {
+      card.style.marginLeft = `${offset * -1}px`
+    })
+  })
+}
