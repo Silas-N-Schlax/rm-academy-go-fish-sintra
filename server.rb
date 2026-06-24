f92a2a1d6bb9f68f316a14b12b1c6c9d33beef05
SilasNS <silasschlax@gmail.com>
2026-06-24 18:39:27 -0400

    Implement basics of api

--- server.rb
+++ server.rb
@@ -1,12 +1,17 @@
 require 'sinatra'
-
+require 'sinatra/contrib/all'
+require 'rack/contrib'
 require_relative 'lib/go_fish/go_fish_game'
 require_relative 'lib/go_fish/player'
 # Server class + Sinatra App
 class Server < Sinatra::Base
   MIN_GAME_SIZE = 2
 
+  register Sinatra::RespondWith
+  use Rack::JSONBodyParser
+
   enable :sessions
+  # set :bind, '0.0.0.0'
   def self.game
     @@game ||= GoFishGame.new
   end
@@ -21,27 +26,35 @@ class Server < Sinatra::Base
   end
 
   get '/' do
-    return redirect '/game' if authenticated?
+    return redirect_to('/game') if authenticate!
 
     slim :login, locals: { errors: nil }
   end
 
   get '/game' do
-    return redirect '/' unless authenticated?
-    return redirect '/game-over' if game.winner
+    return redirect_to('/') unless authenticate!
+    return redirect_to('/game-over') if game.winner
 
     start_game_if_possible
-    slim :game, locals: { game: game, current_player: game.find_player(current_player_name) }
+    respond_to do |f|
+      f.html { slim :game, locals: { game: game, current_player: game.find_player(current_player_name) } }
+      f.json { json game.as_json(current_json_player_name) }
+    end
   end
 
   post '/join' do
-    return redirect '/wrong-name' unless name_valid?(params[:name])
+    name = params[:name] || JSON.parse(request.body.read)['name']
+    return redirect '/wrong-name' unless name_valid?(name)
 
-    api_key = Base64.urlsafe_encode64("#{params[:name]}:#{Time.new.to_f}")
+    api_key = Base64.urlsafe_encode64("#{name}:#{Time.new.to_f}")
     session[:api_key] = api_key
-    self.class.api_keys[api_key] = params[:name]
-    game.add_player(params[:name])
-    redirect '/game'
+    self.class.api_keys[api_key] = name
+    game.add_player(name)
+
+    respond_to do |f|
+      f.html { redirect 'game' }
+      f.json { { 'api_key' => api_key }.to_json }
+    end
   end
 
   get '/wrong-name' do
@@ -59,16 +72,50 @@ class Server < Sinatra::Base
   end
 
   post '/ask' do
-    return redirect '/' unless authenticated?
-    return redirect '/game' unless game.started?
+    return redirect_to('/') unless authenticate!
+    return redirect_to('/game') unless game.started?
 
-    return redirect '/wrong-turn' unless current_player_name == game.current_player.name
+    return redirect('/wrong-turn') unless current_player_name == game.current_player.name
 
     game.run_turn(params[:players], params[:ranks])
     redirect '/game'
   end
 
   private
+
+  def authenticate!
+    return api_auth? if auth.provided?
+
+    web_auth?
+
+    # auth = Rack::Auth::Basic::Request.new(request.env)
+
+    # return nil unless auth.provided? && auth.basic?
+
+    # api_keys = auth.username
+  end
+
+  def redirect_to(path)
+    respond_to do |f|
+      f.html { redirect path }
+      f.json { halt 400 }
+    end
+  end
+
+  def api_auth?
+    halt 401 unless api_keys[auth.username]
+    true
+  end
+
+  def web_auth?
+    return true if current_player_name
+
+    false
+  end
+
+  def auth
+    Rack::Auth::Basic::Request.new(request.env)
+  end
 
   def name_valid?(name)
     return false if name.empty?
@@ -77,14 +124,13 @@ class Server < Sinatra::Base
     true
   end
 
-  def authenticated?
-    return true if current_player_name
-
-    false
-  end
-
   def current_player_name
     self.class.api_keys[session[:api_key]]
+    # ! Shorten with api_keys method
+  end
+
+  def current_json_player_name
+    api_keys[auth.username]
   end
 
   def game
---
+++ spec/views/api_spec.rb
@@ -0,0 +1,54 @@
+require_relative '../../server'
+require_relative '../../lib/go_fish/card'
+
+describe Server, type: :request do
+  after(:each) do
+    Server.reset!
+  end
+
+  include Rack::Test::Methods
+
+  def app
+    Server.new
+  end
+
+  it 'returns a response matching the join schema' do
+    post '/join', { 'name' => 'Bot' }.to_json, { 'HTTP_ACCEPT' => 'application/json', 'CONTENT_TYPE' => 'application/json' }
+    expect(last_response).to be_ok
+    expect(last_response).to match_json_schema('join')
+  end
+
+  describe 'GET /game' do
+    it 'prevents unauthorized requests' do
+      get '/game', {}, http_header('')
+      expect(last_response.status).to eq 401
+    end
+
+    it 'allows authorized requests' do
+      encoded = join_game
+      get '/game', {}, http_header(encoded)
+      expect(last_response).to be_ok
+    end
+  end
+
+  it 'returns a response matching the game schema' do
+    encoded = join_game
+    get '/game', {}, http_header(encoded)
+    expect(last_response).to be_ok
+    expect(last_response.body).to match_json_schema('game')
+  end
+
+  def http_header(encoded)
+    {
+      'HTTP_ACCEPT' => 'application/json',
+      'CONTENT_TYPE' => 'application/json',
+      'HTTP_AUTHORIZATION' => "Basic #{encoded}"
+    }
+  end
+
+  def join_game
+    post '/join', { 'name' => 'Bot' }.to_json, { 'HTTP_ACCEPT' => 'application/json', 'CONTENT_TYPE' => 'application/json' }
+    api_key = JSON.parse(last_response.body)['api_key']
+    Base64.encode64("#{api_key}:X").strip
+  end
+end
